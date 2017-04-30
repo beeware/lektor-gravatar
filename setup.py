c7b0d4e7d88c1c3463ab4398a43ad2e5cd136057
Russell Keith-Magee <russell@keith-magee.com>
2017-04-30 10:18:33 +0800

    Bumped version for v0.1.2 release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lektor-gravatar',
-    version='0.1.1',
+    version='0.1.2',
     author=u'Russell Keith-Magee',
     author_email='russell@keith-magee.com',
     url='https://github.com/pybee/lektor-gravatar',
